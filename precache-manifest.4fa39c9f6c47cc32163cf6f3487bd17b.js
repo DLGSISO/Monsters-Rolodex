6765b77a1235b0523d1f4ba52b9a9c339a6c07b3
stanislav.milkovski@mentormate.com <stanislav.milkovski@mentormate.com>
2020-09-09 12:22:51 +0300

    Updates

---
+++ precache-manifest.4fa39c9f6c47cc32163cf6f3487bd17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4e425a1626781b147bc36009dc2e39",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "ec64cba0f494c28b9fc9",
+    "url": "/Monsters-Rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec64cba0f494c28b9fc9",
+    "url": "/Monsters-Rolodex/static/js/main.8463ee06.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
